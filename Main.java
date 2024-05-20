1d7623f33323ffcd342e79a7402cd7542f7636cc
ValeryChebet <valerychebet.ke@gmail.com>
2024-05-20 21:27:56 +0300

    First Commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]args){
+        int length = 8;
+        int width = 10;
+        int area = (length * width); 
+
+        System.out.println("The lenght is: " + length);
+        System.out.println("The width is: " + width);
+        System.out.println("The area is: " + area);
+
+    }
+}
+
